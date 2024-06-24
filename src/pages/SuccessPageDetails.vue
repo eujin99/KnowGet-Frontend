832f57f3d0f44c86530541e43cb99a6cc77fece1
MJ <ssy03035@gmail.com>
2024-06-24 23:37:11 +0900

    [성공 사례] 성공 사례 검색, 댓글 crud, 답글 조회(real ver.:( )

--- src/pages/SuccessPageDetails.vue
+++ src/pages/SuccessPageDetails.vue
@@ -21,6 +21,7 @@
                 <p>{{ successCase.content }}</p>
               </q-card-section>
 
+              <q-separator spaced />
               <div class="comment-form">
                 <q-input
                   filled
@@ -34,8 +35,7 @@
                 <q-btn color="primary" label="확인" @click="submitComment" />
               </div>
 
-              <div class="comments-section" v-if="comments.values">
-                <br />
+              <div class="comments-section" v-if="comments.length > 0">
                 <div
                   class="comment"
                   v-for="comment in comments"
@@ -44,7 +44,24 @@
                   <div class="comment-writer">
                     작성자 {{ comment.username }}
                   </div>
-                  <div class="comment-content">{{ comment.content }}</div>
+                  <div v-if="!comment.editing" class="comment-content">
+                    {{ comment.content }}
+                  </div>
+                  <div v-if="comment.editing">
+                    <q-input
+                      filled
+                      outlined
+                      dense
+                      v-model="comment.newContent"
+                      :placeholder="comment.content"
+                      clearable
+                    />
+                    <q-btn
+                      color="primary"
+                      label="저장"
+                      @click="saveEditedComment(comment)"
+                    />
+                  </div>
                   <div class="comment-date">
                     작성 시각
                     {{ new Date(comment.createdDate).toLocaleDateString() }}
@@ -52,12 +69,97 @@
                       <q-btn
                         color="primary"
                         label="수정"
-                        @click="editComment(comment.commentId)"
+                        @click="toggleEditing(comment)"
                       />
                       <q-btn
                         color="primary"
                         label="삭제"
-                        @click="deleteComment(comment.commentId)"
+                        @click="confirmDeleteComment(comment.commentId)"
+                      />
+                    </div>
+                  </div>
+
+                  <!-- 답글 버튼 또는 답글 개수 보기 -->
+                  <q-btn
+                    v-if="!comment.showReplies"
+                    color="primary"
+                    :label="
+                      comment.replies.length > 0
+                        ? `${comment.replies.length}개의 답글 보기`
+                        : '답글 보기'
+                    "
+                    @click="toggleShowReplies(comment)"
+                  />
+                  <q-btn
+                    v-else-if="comment.showReplies"
+                    color="primary"
+                    label="답글 숨기기"
+                    @click="toggleShowReplies(comment)"
+                  />
+                  <!-- 답글 목록 -->
+                  <div v-if="comment.showReplies">
+                    <div v-if="comment.replies.length === 0" class="noReply">
+                      현재 해당 댓글의 답글이 없습니다.
+                    </div>
+                    <div
+                      class="reply"
+                      v-for="reply in comment.replies"
+                      :key="reply.replyId"
+                    >
+                      <div class="reply-writer">
+                        작성자 {{ reply.username }}
+                      </div>
+                      <div v-if="!reply.editing" class="reply-content">
+                        {{ reply.content }}
+                      </div>
+                      <div v-if="reply.editing">
+                        <q-input
+                          filled
+                          outlined
+                          dense
+                          v-model="reply.newContent"
+                          :placeholder="reply.content"
+                          clearable
+                        />
+                        <q-btn
+                          color="primary"
+                          label="저장"
+                          @click="saveEditedReply(reply)"
+                        />
+                      </div>
+                      <div class="reply-date">
+                        작성 시각
+                        {{ new Date(reply.createdDate).toLocaleDateString() }}
+                        <div v-if="reply.username === authStore.username">
+                          <q-btn
+                            color="primary"
+                            label="수정"
+                            @click="toggleEditingReply(reply)"
+                          />
+                          <q-btn
+                            color="primary"
+                            label="삭제"
+                            @click="deleteReply(reply.replyId)"
+                          />
+                        </div>
+                      </div>
+                    </div>
+
+                    <!-- 답글 작성 폼 -->
+                    <div class="reply-form">
+                      <q-input
+                        filled
+                        outlined
+                        dense
+                        v-model="newReplyContent[comment.commentId]"
+                        placeholder="답글을 작성해주세요..."
+                        clearable
+                        @keydown.enter="submitReply(comment)"
+                      />
+                      <q-btn
+                        color="primary"
+                        label="확인"
+                        @click="submitReply(comment)"
                       />
                     </div>
                   </div>
@@ -72,7 +174,7 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, reactive, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import { api, customApi } from 'boot/axios';
 import { useAuthStore } from 'stores/authStore';
@@ -80,6 +182,7 @@ import { useAuthStore } from 'stores/authStore';
 const successCase = ref(null);
 const comments = ref([]);
 const newComment = ref('');
+const newReplyContent = reactive({});
 const router = useRouter();
 const caseId = router.currentRoute.value.params.caseId;
 const authStore = useAuthStore();
@@ -95,25 +198,47 @@ const fetchSuccessCase = async () => {
 };
 
 const editComment = async commentId => {
-  const newContent = prompt('댓글을 수정하세요:');
-
   try {
-    const response = await customApi.patch(
-      `/success-case/${caseId}/comment/${commentId}`,
-      {
-        content: newContent,
-      },
-    );
-    console.log('댓글 수정 성공: ', response.data);
+    const newContent = newContent.length();
+    if (newContent.length === 0) {
+      window.alert('댓글 내용을 최소 1자 이상 입력해주세요');
+    } else {
+      const response = await customApi.patch(
+        `/success-case/${caseId}/comment/${commentId}`,
+        { content: newContent },
+      );
 
-    const commentIndex = comments.value.findIndex(
-      comment => comment.commentId === commentId,
-    );
-    if (commentIndex !== -1) {
-      comments.value[commentIndex].content = newContent.trim();
+      console.log('댓글 수정 성공:', response.data.message);
+
+      const commentIndex = comments.value.findIndex(
+        comment => comment.commentId === commentId,
+      );
+
+      if (commentIndex !== -1) {
+        comments.value[commentIndex].content = newContent;
+      }
+
+      // 수정 후 댓글 목록을 다시 불러옴
+      fetchComments();
+
+      // 입력 상자 초기화
+      newCommentForEdit.value = '';
     }
   } catch (error) {
-    console.error('댓글 수정 에러', error);
+    console.error('댓글 수정 에러:', error);
+  }
+};
+const confirmDeleteComment = async commentId => {
+  const comment = comments.value.find(
+    comment => comment.commentId === commentId,
+  );
+
+  if (comment.replies.length > 0) {
+    window.confirm('댓글에 답글이 달려있습니다. 삭제가 불가합니다.');
+  } else {
+    if (window.confirm('정말로 삭제하시겠습니까?')) {
+      deleteComment(commentId);
+    }
   }
 };
 
@@ -123,109 +248,270 @@ const deleteComment = async commentId => {
       `/success-case/${caseId}/comment/${commentId}`,
     );
     console.log('댓글 삭제 성공:', response.data.message);
+
+    fetchComments();
   } catch (error) {
-    console.error('댓글 삭제 에러', error);
+    console.error('댓글 삭제 에러:', error);
   }
 };
 
+// fetchComments 함수 추가
+// 댓글 목록을 불러오는 API를 호출하고, 불러온 댓글 목록을 comments 변수에 저장
 const fetchComments = async () => {
   try {
     const response = await customApi.get(`/success-case/${caseId}/comments`);
-    console.log(response.data);
     if (Array.isArray(response.data)) {
-      comments.value = response.data;
+      const fetchedComments = response.data.map(comment => ({
+        ...comment,
+        editing: false,
+        newContent: '',
+        showReplies: false,
+        replies: [],
+      }));
+
+      // 각 댓글마다 답글 로딩(for n개의 답글 보기 버튼)
+      for (let comment of fetchedComments) {
+        // fetchedComments 배열의 각 요소를 comment로 받아와서 반복문을 실행
+        const replyResponse = await customApi.get(
+          `/comment/${comment.commentId}/replies`,
+        );
+        if (Array.isArray(replyResponse.data)) {
+          // replyResponse.data가 배열이면
+          comment.replies = replyResponse.data.map(reply => ({
+            ...reply,
+            editing: false,
+            newContent: '',
+          }));
+        } else {
+          throw new Error('Invalid API response for replies');
+        }
+      }
+
+      // comments 업데이트
+      comments.value = fetchedComments.sort(
+        (a, b) => new Date(b.createdDate) - new Date(a.createdDate),
+      );
+
+      // newReplyContent 초기화
+      comments.value.forEach(comment => {
+        newReplyContent[comment.commentId] = '';
+      });
     } else {
-      throw new Error('Invalid API response');
+      throw new Error('Invalid API response for comments');
     }
   } catch (error) {
-    console.error('댓글 fetch 에러', error);
+    console.error('댓글 fetch 에러:', error);
   }
 };
 
-onMounted(fetchSuccessCase);
+const toggleEditing = comment => {
+  comment.editing = !comment.editing;
+  if (comment.editing) {
+    comment.newContent = comment.content;
+  }
+};
+
+const saveEditedComment = async comment => {
+  try {
+    const newContent = comment.newContent.trim();
+    if (newContent.length === 0) {
+      console.log('댓글 내용을 최소 1자 이상 입력해주세요');
+      return;
+    }
+
+    const response = await customApi.patch(
+      `/success-case/${caseId}/comment/${comment.commentId}`,
+      { content: newContent },
+    );
+
+    console.log('댓글 수정 성공:', response.data.message);
+
+    // 수정 완료 후 editing 상태 해제
+    comment.editing = false;
+
+    // 수정 후 댓글 목록을 다시 불러옴
+    fetchComments();
+  } catch (error) {
+    console.error('댓글 수정 에러:', error);
+  }
+};
 
 const submitComment = async () => {
-  if (newComment.value.length === 0) {
-    console.log('댓글 내용을 최소 1 자 이상 입력해주세요');
-    return;
-  }
   try {
     const response = await customApi.post(`/success-case/${caseId}/comment`, {
       caseId: caseId,
       content: newComment.value,
     });
+
     console.log('댓글 작성 성공:', response.data.message);
+
+    // 댓글 작성 후 입력 상자 초기화 및 댓글 목록 다시 불러옴
     newComment.value = '';
+    fetchComments();
   } catch (error) {
-    console.error('댓글 작성 에러', error);
+    console.error('댓글 작성 에러:', error);
   }
 };
+
+const toggleShowReplies = async comment => {
+  try {
+    comment.showReplies = !comment.showReplies;
+    if (comment.showReplies && comment.replies.length === 0) {
+      const response = await customApi.get(
+        `/comment/${comment.commentId}/replies`,
+      );
+      if (Array.isArray(response.data)) {
+        comment.replies = response.data.map(reply => ({
+          ...reply,
+          editing: false,
+          newContent: '',
+        }));
+      } else {
+        throw new Error('Invalid API response');
+      }
+    }
+  } catch (error) {
+    console.error('답글 fetch 에러:', error);
+  }
+};
+
+const toggleEditingReply = reply => {
+  reply.editing = !reply.editing;
+  if (reply.editing) {
+    reply.newContent = reply.content;
+  }
+};
+
+const saveEditedReply = async reply => {
+  try {
+    const newContent = reply.newContent.trim();
+    if (newContent.length === 0) {
+      console.log('답글 내용을 최소 1자 이상 입력해주세요');
+      return;
+    }
+
+    const response = await customApi.patch(
+      `/success-case/${caseId}/reply/${reply.replyId}`,
+      { content: newContent },
+    );
+
+    console.log('답글 수정 성공:', response.data.message);
+
+    // 수정 완료 후 editing 상태 해제
+    reply.editing = false;
+
+    // 수정 후 답글 목록을 다시 불러옴
+    fetchComments(); // 답글만 불러오는 API로 변경 필요
+  } catch (error) {
+    console.error('답글 수정 에러:', error);
+  }
+};
+
+const deleteReply = async replyId => {
+  try {
+    const response = await customApi.delete(
+      `/success-case/${caseId}/reply/${replyId}`,
+    );
+
+    console.log('답글 삭제 성공:', response.data.message);
+
+    // 삭제 후 답글 목록을 다시 불러옴
+    fetchComments(); // 답글만 불러오는 API로 변경 필요
+  } catch (error) {
+    console.error('답글 삭제 에러:', error);
+  }
+};
+
+onMounted(fetchSuccessCase);
 </script>
 
 <style scoped>
 .page-wrapper {
   display: flex;
   justify-content: center;
-  align-items: flex-start;
   padding: 20px;
-  box-sizing: border-box;
 }
 
 .page-card {
   width: 100%;
-  max-width: 1200px;
-  margin: 20px auto;
-  padding: 20px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  max-width: 800px;
 }
 
 .header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+  font-size: 20px;
+  font-weight: bold;
 }
 
-.text-caption {
-  font-size: 14px;
-  color: #666;
+.sc-title {
+  font-size: 24px;
 }
 
 .content-card {
-  width: 100%;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  margin-top: 20px;
 }
 
 .content-section {
-  padding: 20px;
+  white-space: pre-wrap;
 }
 
 .comment {
-  margin-bottom: 10px;
-  border-bottom: 1px solid #ccc;
-  padding-bottom: 10px;
-}
-.comment-section {
-  width: 100%;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
-  padding: 20px;
+  margin-top: 20px;
+  padding: 10px;
+  border: 1px solid #ccc;
 }
 
-.comment-item {
-  margin-bottom: 10px;
-  border-bottom: 1px solid #ccc;
-  padding-bottom: 10px;
+.comment-writer {
+  font-weight: bold;
+}
+
+.comment-content {
+  margin-top: 10px;
+}
+
+.comment-date {
+  font-size: 12px;
+  color: #888;
 }
 
 .comment-form {
+  flex: auto;
   margin-top: 20px;
-  margin-left: 20px;
-  width: 120%;
+  margin-left: 10px;
+  margin-right: 10px;
+}
+
+.comments-section {
+  margin-top: 20px;
+}
+
+.reply {
+  margin-top: 10px;
+  padding: 10px;
+  border: 1px solid #eee;
+}
+
+.reply-writer {
+  font-weight: bold;
+}
+
+.reply-content {
+  margin-top: 10px;
+}
+
+.reply-date {
+  font-size: 12px;
+  color: #888;
+}
+
+.reply-form {
+  margin-top: 10px;
   display: flex;
   align-items: center;
 }
-.comment-date {
-  font-size: 12px;
-  justify-content: space-between;
-  color: #666;
+.noReply {
+  margin-top: 10px;
+  padding: 10px;
+  border: 1px solid #eee;
+  text-align: center;
 }
 </style>
